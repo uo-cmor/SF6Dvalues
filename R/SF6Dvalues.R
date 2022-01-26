a8e0fc996c15d97b8d7e16efdf43ea16d8e67b9a
Ross Wilson <ross.wilson@otago.ac.nz>
2022-01-27 11:05:02 +1300

    add package level documentation

---
+++ R/SF6Dvalues.R
@@ -0,0 +1,8 @@
+#' SF6Dvalues: A package to compute SF-12/36 scores and SF-6D utility values
+#'
+#' This package provides various tools for calculation of SF-6D profiles,
+#' PCS/MCS scores, and health domain scores from SF-12 or SF-36 responses.
+#'
+#' @docType package
+#' @name SF6Dvalues
+NULL
